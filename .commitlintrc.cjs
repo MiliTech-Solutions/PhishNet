5a265d5a734dd0470669c93822e64ae02d70a954
gh0st-bit <27668@students.riphah.edu.pk>
2025-08-27 23:30:50 +0500

    chore(commitlint): remove unsupported header-pattern rule

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -2,6 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'refactor', 'test', 'ci', 'build']],
-    'header-pattern': [2, 'always', /^(feat|fix|chore|docs|refactor|test|ci|build)(\([^)]*\))? PHN-\d+: .+/]
+  // Removed custom header-pattern rule (unsupported in current commitlint version)
   }
 };
